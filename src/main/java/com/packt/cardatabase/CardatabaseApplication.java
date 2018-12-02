e5ff102e821f29891bb5981d4da0778ed7ad14db
Mick Gallagher <mickster.gallagher@gmail.com>
2018-12-02 15:05:54 -0800

    Adding a logging statement

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -4,14 +4,20 @@ import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
 import com.packt.cardatabase.domain.Owner;
 import com.packt.cardatabase.domain.OwnerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
 @SpringBootApplication
 public class CardatabaseApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class.getName());
+
 	@Autowired
 	private CarRepository repository;
 
@@ -20,6 +26,8 @@ public class CardatabaseApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
+
+		logger.debug("Mick's cardatabase with REACT front-end running...");
 	}
 	
 	@Bean
