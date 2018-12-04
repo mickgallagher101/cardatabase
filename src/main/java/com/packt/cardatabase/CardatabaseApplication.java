4712a21fc4165752c16a95059e8e092e72b3675c
Mick Gallagher <mickster.gallagher@gmail.com>
2018-12-03 16:07:34 -0800

    Adding some funny data

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -41,6 +41,9 @@ public class CardatabaseApplication {
 			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
 			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
 			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
+			repository.save(new Car("Robin", "Reliant", "Gold", "KKO-0212", 1979, 25, owner2));
+			repository.save(new Car("Nissan", "Xterra", "Blue", "ABC-1234", 2001, 1000, owner1));
+
 		};
 	}	
 }
