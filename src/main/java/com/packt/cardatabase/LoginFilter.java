2b0a497797a690e7aacad70cb35be21cf4bcd834
Mick Gallagher <mickster.gallagher@gmail.com>
2018-12-04 19:17:28 -0800

    Formatting changes

--- src/main/java/com/packt/cardatabase/LoginFilter.java
+++ src/main/java/com/packt/cardatabase/LoginFilter.java
@@ -19,31 +19,31 @@ import java.util.Collections;
 
 public class LoginFilter extends AbstractAuthenticationProcessingFilter {
 
-  public LoginFilter(String url, AuthenticationManager authManager) {
-    super(new AntPathRequestMatcher(url));
-    setAuthenticationManager(authManager);
-  }
+    public LoginFilter(String url, AuthenticationManager authManager) {
+        super(new AntPathRequestMatcher(url));
+        setAuthenticationManager(authManager);
+    }
 
-  @Override
-  public Authentication attemptAuthentication(
-      HttpServletRequest req, HttpServletResponse res)
-			throws AuthenticationException, IOException, ServletException {
-	AccountCredentials creds = new ObjectMapper()
-        .readValue(req.getInputStream(), AccountCredentials.class);
-	return getAuthenticationManager().authenticate(
-        new UsernamePasswordAuthenticationToken(
-            creds.getUsername(),
-            creds.getPassword(),
-            Collections.emptyList()
-        )
-    );
-  }
+    @Override
+    public Authentication attemptAuthentication(
+        HttpServletRequest req, HttpServletResponse res)
+        throws AuthenticationException, IOException, ServletException {
+        AccountCredentials creds = new ObjectMapper()
+            .readValue(req.getInputStream(), AccountCredentials.class);
+        return getAuthenticationManager().authenticate(
+            new UsernamePasswordAuthenticationToken(
+                creds.getUsername(),
+                creds.getPassword(),
+                Collections.emptyList()
+            )
+        );
+    }
 
-  @Override
-  protected void successfulAuthentication(
-      HttpServletRequest req,
-      HttpServletResponse res, FilterChain chain,
-      Authentication auth) throws IOException, ServletException {
-	  AuthenticationService.addToken(res, auth.getName());
-  }
+    @Override
+    protected void successfulAuthentication(
+        HttpServletRequest req,
+        HttpServletResponse res, FilterChain chain,
+        Authentication auth) throws IOException, ServletException {
+        AuthenticationService.addToken(res, auth.getName());
+    }
 }
